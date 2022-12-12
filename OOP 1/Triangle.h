8bf926e5b0a66256abd0e1eb30c15e22e7844dca
ebatuhan <120257510+ebatuhan@users.noreply.github.com>
2022-12-12 03:56:28 +0300

    working on triangles

--- OOP 1/Triangle.h
+++ OOP 1/Triangle.h
@@ -1,1 +1,13 @@
 #pragma once
+#include "Point.h"
+
+class Triangle 
+{
+private:
+	Point a_, b_, c_;
+
+public:
+	Triangle();
+	Triangle(double a_, double b_, double c_);
+	Triangle(const Triangle& other);
+};
